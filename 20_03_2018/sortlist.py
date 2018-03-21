545bd211953c152f5735a30010002fd437339a59
Rui Ramos <ruimsr79@gmail.com>
2018-03-21 20:42:30 +0000

    Alteração no código
    Foram adicionadas as chamadas das funções.
    
    Co-Authored-By: ruimsr79 <ruimsr79@users.noreply.github.com>

--- 20_03_2018/sortlist.py
+++ 20_03_2018/sortlist.py
@@ -8,14 +8,18 @@ def linear_search(mylist, find):
 def binary_search(mylist, find):
     while len(mylist) > 0:
         mid = (len(mylist))//2
+        print('O mid é {}'.format(mid))
         if mylist[mid] == find:
+            print('O número {} encontrado!'.format(find))
             return True
         elif find < mylist[mid]:
+            print('O número está à esquerda da lista {}!'.format(mylist))
             mylist = mylist[:mid]
         else:
             mylist = mylist[mid + 1:]
+            print('O número está à direita da lista {}! {}'.format(mylist,mid))
     return False
-
-
-mylist = [1, 2, 3, 4, 5, 6, 7]
-find = [10, 12]
+if __name__ == '__main__':
+    list = [x + 1 for x in range (100)]
+    linear_search(list, 19)
+    binary_search(list, 19)
